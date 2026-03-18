60819134ad82d41ade52d6e8e4c6310168f136fc
mcharur1 <margarita.arias@stud.hslu.ch>
2026-03-18 15:50:59 +0100

    Merged dataset to work on data quality checks.

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -6,53 +6,51 @@ import pandas as pd
 # git commit -m "message"
 # git push
 
-# THIS IS THE NEW SCRIPT
-
-a = "some varibale"
-
 # Margarita: [1] Merge Data, [2] Check Data types
 # Svenja: [3] check and handle missing values
 # Michael: [4] outlier check
 # All three: [5] visualize -> think research questions...
 
 # Load and pre-process GDP data
-df_gdp = pd.read_csv('data/processed/immobilier_all_cantons_allpages_snapshot_p20.csv')
-df_gdp = df_gdp.rename(columns={'Canton': 'canton_name'})
+df_gdp = pd.read_csv('data/processed/canton_gdp_2022_clean.csv')
+df_gdp = df_gdp.rename(columns={'Canton': 'canton_name', '2022': 'canton_gdp'})
 
 # Add abbreviated canton column to GDP for later join
 CANTONS = {
-    "ZH": "zurich",
-    "BE": "berne",
-    "LU": "lucerne",
-    "UR": "uri",
-    "SZ": "schwyz",
-    "OW": "obwald",
-    "NW": "nidwald",
-    "GL": "glaris",
-    "ZG": "zoug",
-    "FR": "fribourg",
-    "SO": "soleure",
-    "BS": "bale-ville",
-    "BL": "bale-campagne",
-    "SH": "schaffhouse",
-    "AR": "appenzell-rhodes-exterieures",
-    "AI": "appenzell-rhodes-interieures",
-    "SG": "st-gall",
-    "GR": "grisons",
-    "AG": "argovie",
-    "TG": "thurgovie",
-    "TI": "tessin",
-    "VD": "vaud",
-    "VS": "valais",
-    "NE": "neuchatel",
-    "GE": "geneva",
-    "JU": "jura",
+    "ZH": "Zurich",
+    "BE": "Berne",
+    "LU": "Lucerne",
+    "UR": "Uri",
+    "SZ": "Schwyz",
+    "OW": "Obwalden",
+    "NW": "Nidwalden",
+    "GL": "Glarus",
+    "ZG": "Zug",
+    "FR": "Fribourg",
+    "SO": "Solothurn",
+    "BS": "Basel-Stadt",
+    "BL": "Basel-Landschaft",
+    "SH": "Schaffhausen",
+    "AR": "Appenzell A. Rh.",
+    "AI": "Appenzell I. Rh.",
+    "SG": "St. Gallen",
+    "GR": "Graubünden",
+    "AG": "Aargau",
+    "TG": "Thurgau",
+    "TI": "Ticino",
+    "VD": "Vaud",
+    "VS": "Valais",
+    "NE": "Neuchâtel",
+    "GE": "Geneva",
+    "JU": "Jura",
+    "CH": "Switzerland"
 }
-print(df_gdp['Canton'].unique())
 
-df = pd.merge(df_listings, df_gdp, on'Canton', how='left')
+df_cantons = pd.DataFrame(list(CANTONS.items()), columns=['canton', 'canton_name'])
+df_gdp = pd.merge(df_gdp, df_cantons, on='canton_name', how='left')
 
 # Load and pre-process listing data
-df_listings = pd.read_csv('/Users/mcharur1/PycharmProjects/CIP/W4/immobilier_all_cantons_allpages_snapshot_p20.csv')
+df_listings = pd.read_csv('data/processed/immobilier_all_cantons_allpages_snapshot_p20.csv')
 
-# Merge GDP and Listings data
+# Merge Listing and GDP data
+df = pd.merge(df_listings, df_gdp, on='canton', how='left')
